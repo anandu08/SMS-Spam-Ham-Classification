2bdd721c36356de822ef440e6e2668bf3d35f58a
anandusateesh <76695947+anandusateeshNSUT@users.noreply.github.com>
2023-04-26 18:41:09 +0530

    Update spam_ham.py

--- spam_ham.py
+++ spam_ham.py
@@ -1,11 +1,25 @@
 import joblib
-model=joblib.load('model.joblib')
-text="You have won 100000"
+import string
+from nltk.corpus import stopwords
+import streamlit as st
+model = joblib.load('model.joblib')
+text = st.text_input('Enter text you want to classify: ')
+st.title('Spam or Ham')
+# Define a function to process text data
+def text_process(mess):
+    STOPWORDS = stopwords.words('english')
+    nopunc = [char for char in mess if char not in string.punctuation]
+    nopunc = ''.join(nopunc)
+    return ' '.join([word for word in nopunc.split() if word.lower() not in STOPWORDS])
+
+# Define a function to make predictions on new data
 def predict_spam_ham(text):
-    text=text_process(text)
-    pred=model.predict([text])[0]
-    if pred==0:
+    text = text_process(text)
+    print(text)
+    pred = model.predict([text])[0]
+    if pred == 0:
         return 'ham'
     else:
         return 'spam'
-print(predict_spam_ham)
+if st.button('Predict'):
+    st.success(predict_spam_ham(text))
